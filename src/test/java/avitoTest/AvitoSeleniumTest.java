d28bf5f6fa2e3048f1aa22144b11d75456c0ab65
grantkar <hrant87@inbox.ru>
2021-06-30 19:52:47 +0400

    Исправление версии плагина maven

--- src/test/java/avitoTest/AvitoSeleniumTest.java
+++ src/test/java/avitoTest/AvitoSeleniumTest.java
@@ -40,6 +40,11 @@ public class AvitoSeleniumTest {
         methodTest.setSearch(ConfProperties.getProperty("search"));
         methodTest.setElementLocal();
         methodTest.setElementCitySearch(ConfProperties.getProperty("citySearch"));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         methodTest.setElementCity();
         methodTest.setElementClickCity();
         resultTest.setElementCheckBox();
